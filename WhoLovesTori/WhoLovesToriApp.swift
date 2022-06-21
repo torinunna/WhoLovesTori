97aa4b7f0829abc73a82cb022fd4a838cea9e1af
torinunna <torinunna9588@gmail.com>
2022-06-21 18:44:33 +0900

    Initial Commit

---
+++ WhoLovesTori/WhoLovesToriApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoLovesToriApp.swift
+//  WhoLovesTori
+//
+//  Created by 권유진 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WhoLovesToriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
